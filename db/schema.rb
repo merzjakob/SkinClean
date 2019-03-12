89f8d1cac384c54c25b93a55613e0f27f0a69cd9
Jakob Merz <jakob.merz@whu.edu>
2019-03-12 11:14:13 +0100

    add picture model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_101203) do
+ActiveRecord::Schema.define(version: 2019_03_12_101301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_03_12_101203) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_doctors_on_user_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "diagnosis_picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
