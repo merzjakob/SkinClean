0f30e120a3f782cfef9b1f74f2b2701f35b4e4c1
Jakob Merz <jakob.merz@whu.edu>
2019-03-12 11:12:43 +0100

    add doctor table

--- db/migrate/20190312092852_create_patients.rb
+++
@@ -1,9 +0,0 @@
-class CreatePatients < ActiveRecord::Migration[5.2]
-  def change
-    create_table :patients do |t|
-      t.references :user, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_092852) do
+ActiveRecord::Schema.define(version: 2019_03_12_101203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "patients", force: :cascade do |t|
+  create_table "doctors", force: :cascade do |t|
+    t.text "introduction"
+    t.string "profile_picture"
+    t.string "license"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_patients_on_user_id"
+    t.index ["user_id"], name: "index_doctors_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,5 +40,5 @@ ActiveRecord::Schema.define(version: 2019_03_12_092852) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "patients", "users"
+  add_foreign_key "doctors", "users"
 end
