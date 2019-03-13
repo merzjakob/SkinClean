cfec455256131fdecb105d6319874ef6458a9430
AntoineMassot <antoinemassot77@gmail.com>
2019-03-13 18:59:18 +0100

    error with the current-patient-answer-content

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,10 @@
 class QuestionsController < ApplicationController
   skip_before_action :authenticate_user!
 
+
   def index
     @questions = Question.all
+    @patient_answer = PatientAnswer.new
   end
 
 end
--- app/models/patient_answer.rb
+++ app/models/patient_answer.rb
@@ -2,4 +2,5 @@ class PatientAnswer < ApplicationRecord
   belongs_to :question
   belongs_to :user
   belongs_to :diagnosis, optional:true
+  mount_uploader :photo, PhotoUploader
 end
