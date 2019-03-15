37a7d3c7757a2256f01e50a0384d9777d57156b5
Jakob Merz <jakob.merz@whu.edu>
2019-03-15 16:52:33 +0100

    patch

--- app/javascript/components/radio_buttons.js
+++ app/javascript/components/radio_buttons.js
@@ -1,7 +1,15 @@
 const activeButtons = () => {
   if (document.querySelector('.chatBottom')) {
     $(document).ready(function(){
-      document.querySelector(".chatBottom").scrollIntoView()
+      const lastBubble = document.querySelector(".chatMessages > .speech-wrapper > div:last-child");
+
+      if (lastBubble) {
+        lastBubble.scrollIntoView()
+      } else {
+        document.querySelector(".chatBottom").scrollIntoView()
+      }
+
+
       $(".form-check").click(function(){
         $(".active").removeClass("active");
         $(this).toggleClass("active");
