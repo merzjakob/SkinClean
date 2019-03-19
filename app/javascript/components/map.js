2d5263409bf16c0e91fa7b65d7548a6d750f05cb
AntoineMassot <antoinemassot77@gmail.com>
2019-03-19 11:53:16 +0100

    the marker popup is bit nicer and it links to the doctor profile

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -7,4 +7,12 @@ const scrollToMapBottom = () => {
     }
   }
 };
+
+// new mapboxgl.Marker(el)
+//   .setLngLat(marker.geometry.coordinates)
+//   .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+//     .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+//   .addTo(map);
+
 export { scrollToMapBottom };
+
